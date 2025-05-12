24d9c7ff8057ab237a332acf5573335a78f434d0
Alex <25120076+WenHsuanYu@users.noreply.github.com>
2025-05-12 20:31:20 +0800

    Refactor q_insert_tail to reuse q_insert_head
    - Simplify the q_insert_tail implementation by directly calling
      q_insert_head with head->prev as the insertion point.
    - This reduces code duplication and leverages the existing logic for
      node creation and insertion.
    
    Change-Id: I20985693fb0b0c831f733bffbf570561c76b0ba0

--- queue.c
+++ queue.c
@@ -69,15 +69,7 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    if (!head)
-        return false;
-
-    element_t *item = q_new_element(s);
-    if (!item)
-        return false;
-
-    list_add_tail(&item->list, head);
-    return true;
+    return q_insert_head(head->prev, s);
 }
 
 /* Remove an element from head of queue */
