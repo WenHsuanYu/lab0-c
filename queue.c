551376357fa53ad190e35ec6cbca10f293c7a379
Alex <25120076+WenHsuanYu@users.noreply.github.com>
2025-05-12 12:07:08 +0800

    Implement q_free function
    - Free all nodes by queue via 'q_release_element' inside the
      list_for_each _safe loop.
    
    Change-Id: I912228790ef02f61e95d57190f22a1a2cf5d5ea8

--- queue.c
+++ queue.c
@@ -16,7 +16,20 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *pos, *safe;
+
+    list_for_each_safe(pos, safe, head) {
+        element_t *item = list_entry(pos, element_t, list);
+        q_release_element(item);
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
